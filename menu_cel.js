6c170f0648f36ae7fde5d9eea3bf8c6948d49a46
steehrodrigues <stefanyrodrigues99@hotmail.com>
2017-10-24 15:52:13 -0200

    Aprimorado o layout para celular

---
+++ menu_cel.js
@@ -0,0 +1,7 @@
+﻿function openNav() {
+    document.getElementById("MenuCelular").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("MenuCelular").style.width = "0";
+}
